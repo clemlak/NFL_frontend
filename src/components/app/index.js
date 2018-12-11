24d58cf6371992da19cd316c6b8d04f66c67d7db
Clement Lakhal <clementlakhal@gmail.com>
2018-12-11 09:15:40 +0100

    Added router

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import Home from '../home';
+import DisplayCard from '../displayCard';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const {
+      address,
+      contract,
+      web3,
+    } = this.props;
+
+    this.state = {
+      address,
+      contract,
+      web3,
+    };
+  }
+
   render() {
+    const {
+      address,
+      contract,
+      web3,
+    } = this.state;
+
     return (
-      <div>
-        <h1>Hello!</h1>
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" render={() => (<Home address={address} contract={contract} web3={web3} />)} />
+            <Route exact path="/card/:cardId" render={props => (<DisplayCard {...props} address={address} contract={contract} web3={web3} />)} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
+
+App.propTypes = {
+  address: PropTypes.string.isRequired,
+  contract: PropTypes.any.isRequired,
+  web3: PropTypes.any.isRequired,
+};
 
 export default App;
