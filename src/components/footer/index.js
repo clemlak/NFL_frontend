6f15d2ecc629287696d6927d1643cc1f170ee1e8
Clement Lakhal <clementlakhal@gmail.com>
2018-12-12 09:42:51 +0100

    Updated Footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,13 +6,93 @@ import {
   Container,
   Row,
   Col,
+  Nav,
+  NavItem,
 } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+  faDiscord,
+  faTelegram,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => (
-  <Container>
-    <Row>
+  <Container fluid className="footer py-3">
+    <Row className="py-2">
       <Col>
-        Footer
+        <Nav vertical>
+          <NavItem>
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/cards">
+              My cards
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/marketplace">
+              Marketplace
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Col>
+      <Col>
+        <Nav vertical>
+          <NavItem>
+            <NavLink className="nav-link" to="/marketplace">
+              Team and careers
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/marketplace">
+              Developers
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/marketplace">
+              FAQ
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Col>
+      <Col>
+        <Nav vertical>
+          <NavItem>
+            <NavLink className="nav-link" to="/marketplace">
+              Privacy
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/marketplace">
+              Terms of Use
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/marketplace">
+              Contact us
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Col>
+    </Row>
+    <Row className="py-2">
+      <Col className="text-center">
+        <FontAwesomeIcon icon={faTwitter} size="2x" className="mx-1" />
+        <FontAwesomeIcon icon={faFacebook} size="2x" className="mx-1" />
+        <FontAwesomeIcon icon={faInstagram} size="2x" className="mx-1" />
+        <FontAwesomeIcon icon={faDiscord} size="2x" className="mx-1" />
+        <FontAwesomeIcon icon={faTelegram} size="2x" className="mx-1" />
+      </Col>
+    </Row>
+    <Row className="py-2">
+      <Col className="text-center">
+        <small>
+          © 2018 Hercules SEZC. NFL Enterprises LLC. NFL and the NFL shield design are registered trademarks of the National Football League. The team names, logos and uniform designs are registered trademarks of the teams indicated. All other NFL-related trademarks are trademarks of the National Football League.
+        </small>
       </Col>
     </Row>
   </Container>
