7f50b40514bb1060f714232b53b16acb600fdc27
Clement Lakhal <clementlakhal@gmail.com>
2018-12-12 10:49:30 +0100

    Updated Footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -20,8 +20,8 @@ import {
 
 const Footer = () => (
   <Container fluid className="footer py-3">
-    <Row className="py-2">
-      <Col>
+    <Row className="py-3 justify-content-center">
+      <Col xs="12" sm="3" md="3" lg="3">
         <Nav vertical>
           <NavItem>
             <NavLink className="nav-link" to="/">
@@ -40,21 +40,26 @@ const Footer = () => (
           </NavItem>
         </Nav>
       </Col>
-      <Col>
+      <Col xs="12" sm="3" md="3" lg="3">
         <Nav vertical>
-          <NavItem>
-            <NavLink className="nav-link" to="/team">
-              Team
-            </NavLink>
-          </NavItem>
           <NavItem>
             <NavLink className="nav-link" to="/faq">
               FAQ
             </NavLink>
           </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/about">
+              About
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/team">
+              Team
+            </NavLink>
+          </NavItem>
         </Nav>
       </Col>
-      <Col>
+      <Col xs="12" sm="3" md="3" lg="3">
         <Nav vertical>
           <NavItem>
             <NavLink className="nav-link" to="/privacy">
@@ -74,17 +79,27 @@ const Footer = () => (
         </Nav>
       </Col>
     </Row>
-    <Row className="py-2">
+    <Row className="py-3 justify-content-center">
       <Col className="text-center">
-        <FontAwesomeIcon icon={faTwitter} size="2x" className="mx-1" />
-        <FontAwesomeIcon icon={faFacebook} size="2x" className="mx-1" />
-        <FontAwesomeIcon icon={faInstagram} size="2x" className="mx-1" />
-        <FontAwesomeIcon icon={faDiscord} size="2x" className="mx-1" />
-        <FontAwesomeIcon icon={faTelegram} size="2x" className="mx-1" />
+        <a href="https://www.twitter.com/" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+          <FontAwesomeIcon icon={faTwitter} size="2x" className="mx-1" />
+        </a>
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+          <FontAwesomeIcon icon={faFacebook} size="2x" className="mx-1" />
+        </a>
+        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+          <FontAwesomeIcon icon={faInstagram} size="2x" className="mx-1" />
+        </a>
+        <a href="https://www.discord.com/" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+          <FontAwesomeIcon icon={faDiscord} size="2x" className="mx-1" />
+        </a>
+        <a href="https://www.telegram.com/" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+          <FontAwesomeIcon icon={faTelegram} size="2x" className="mx-1" />
+        </a>
       </Col>
     </Row>
-    <Row className="py-2">
-      <Col className="text-center">
+    <Row className="py-3 justify-content-center">
+      <Col xs="12" sm="10" md="10" lg="10" className="text-center">
         <small>
           © 2018 Hercules SEZC. NFL Enterprises LLC. NFL and the NFL shield design are registered trademarks of the National Football League. The team names, logos and uniform designs are registered trademarks of the teams indicated. All other NFL-related trademarks are trademarks of the National Football League.
         </small>
