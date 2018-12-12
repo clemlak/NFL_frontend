2be303a99a724ebfe8b3969d24c24c42086cb30d
Clement Lakhal <clementlakhal@gmail.com>
2018-12-12 09:42:40 +0100

    Added About page

---
+++ src/components/about/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+
+const About = () => (
+  <Container>
+    <Row>
+      <Col>
+        <h1>Team</h1>
+        <p>Our team</p>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default About;
