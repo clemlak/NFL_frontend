5e564107d7068b84a8eb6e341501b6f6b1044075
Clement Lakhal <clementlakhal@gmail.com>
2018-12-12 09:43:07 +0100

    Added Terms page

---
+++ src/components/terms/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+
+const Terms = () => (
+  <Container>
+    <Row>
+      <Col>
+        <h1>Terms</h1>
+        <p>Our Terms of Use</p>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Terms;
