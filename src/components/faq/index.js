c733c2c342d9c404f5c537efb48f2094d53e0994
Clement Lakhal <clementlakhal@gmail.com>
2018-12-12 09:42:45 +0100

    Added FAQ page

---
+++ src/components/faq/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+
+const Faq = () => (
+  <Container>
+    <Row>
+      <Col>
+        <h1>Frequently Asked Questions</h1>
+        <p>Random question?</p>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Faq;
