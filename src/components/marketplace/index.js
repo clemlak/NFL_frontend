3ffaad6e9a7569d7dfd4b6970d891b35711871bb
Clement Lakhal <clementlakhal@gmail.com>
2018-12-12 09:42:57 +0100

    Added Marketplace page

---
+++ src/components/marketplace/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+
+const Marketplace = () => (
+  <Container>
+    <Row>
+      <Col>
+        <h1>Team</h1>
+        <p>Our team</p>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Marketplace;
