90661bd4e1203c33347a01ecda5a275fc872dcca
Clement Lakhal <clementlakhal@gmail.com>
2018-12-12 10:49:23 +0100

    Updated page

--- src/components/faq/index.js
+++ src/components/faq/index.js
@@ -7,10 +7,28 @@ import {
 
 const Faq = () => (
   <Container>
-    <Row>
-      <Col>
+    <Row className="py-3 justify-content-center">
+      <Col xs="12" sm="10" md="10" lg="10">
         <h1>Frequently Asked Questions</h1>
-        <p>Random question?</p>
+        <p className="lead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </Col>
+    </Row>
+    <Row className="py-3 justify-content-center">
+      <Col xs="12" sm="10" md="10" lg="10">
+        <h3>Praesent at luctus purus?</h3>
+        <p>
+          Nunc iaculis interdum felis, ac lacinia leo molestie id. Morbi nec odio nulla. Duis et nibh at massa egestas aliquet nec non lorem.
+        </p>
+      </Col>
+    </Row>
+    <Row className="py-3 justify-content-center">
+      <Col xs="12" sm="10" md="10" lg="10">
+        <h3>Nunc finibus nunc id sapien tristique sollicitudin?</h3>
+        <p>
+          Sed eget ipsum id arcu rutrum efficitur. Quisque enim nisl, posuere id lorem in, tempus efficitur tellus. Aenean sed viverra nulla. Sed eget ipsum id arcu rutrum efficitur.
+        </p>
       </Col>
     </Row>
   </Container>
