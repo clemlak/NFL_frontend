7a7a1407e26bccdce01e97ac6c2f9e0415391037
Clement Lakhal <clementlakhal@gmail.com>
2018-12-14 11:48:12 +0100

    Updated

--- src/components/displayCard/index.js
+++ src/components/displayCard/index.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import IPFS from 'ipfs-http-client';
 import {
   Container,
   Row,
   Col,
   Card,
   CardBody,
-  CardHeader,
-  CardFooter,
   ListGroup,
   ListGroupItem,
+  Button,
 } from 'reactstrap';
+
+import RarityBadge from '../rarityBadge';
+
+import ImgPlaceholder from '../../common/img/imgPlaceholder.png';
 
 class DisplayCard extends Component {
   constructor(props) {
     super(props);
+
+    this.ipfs = IPFS({
+      host: 'ipfs.infura.io',
+      port: 5001,
+      protocol: 'https',
+    });
 
     const {
       address,
@@ -36,7 +46,9 @@ class DisplayCard extends Component {
       teamId: 0,
       rarity: 0,
       createdAt: 0,
-      cardOwner: '',
+      bio: '',
+      owner: '',
+      uri: ImgPlaceholder,
       isCardLoaded: false,
     };
   }
@@ -58,7 +70,31 @@ class DisplayCard extends Component {
           name: card[0],
           teamId: card[1],
           rarity: card[2],
-          createdAt: card[3],
+          createdAt: card[4],
+        });
+
+        return this.ipfs.get(card[3]);
+      })
+      .then((files) => {
+        const raw = files[0].content;
+        const data = JSON.parse(raw.toString('utf8'));
+
+        this.setState({
+          bio: data.bio,
+        });
+
+        return contract.methods.tokenURI(cardId).call();
+      })
+      .then((uri) => {
+        this.setState({
+          uri,
+        });
+
+        return contract.methods.ownerOf(cardId).call();
+      })
+      .then((owner) => {
+        this.setState({
+          owner,
           isCardLoaded: true,
         });
       })
@@ -92,62 +128,101 @@ class DisplayCard extends Component {
       rarity,
       createdAt,
       isCardLoaded,
+      bio,
+      uri,
+      owner,
     } = this.state;
 
     if (isCardLoaded) {
       return (
-        <ListGroup>
-          <ListGroupItem>
-            {`Card id: ${cardId}`}
-          </ListGroupItem>
-          <ListGroupItem>
-            {`Card name: ${name}`}
-          </ListGroupItem>
-          <ListGroupItem>
-            {`Card team Id: ${teamId}`}
-          </ListGroupItem>
-          <ListGroupItem>
-            {`Card rarity: ${rarity}`}
-          </ListGroupItem>
-          <ListGroupItem>
-            {`Card created at: ${createdAt}`}
-          </ListGroupItem>
-        </ListGroup>
+        <div>
+          <Row className="py-3 justify-content-center">
+            <Col className="text-center" xs="12" sm="10" md="8" lg="8">
+              <img src={uri} alt="img" />
+            </Col>
+          </Row>
+          <Row className="py-3 justify-content-center">
+            <Col xs="6" sm="5" md="4" lg="4">
+              <h1>
+                {name}
+                {' '}
+                <RarityBadge rarity={rarity} />
+              </h1>
+              <p>
+                Card #
+                {cardId}
+                {' '}
+                owned by
+                {' '}
+                {owner}
+              </p>
+            </Col>
+            <Col className="text-right" xs="6" sm="5" md="4" lg="4">
+              <p>
+                {teamId}
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3 justify-content-center">
+            <Col xs="12" sm="10" md="8" lg="8">
+              <h3>Bio</h3>
+              <p>
+                {bio}
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3 justify-content-center">
+            <Col xs="12" sm="10" md="8" lg="8">
+              <ListGroup>
+                <ListGroupItem>
+                  {`Card created at: ${createdAt}`}
+                </ListGroupItem>
+              </ListGroup>
+            </Col>
+          </Row>
+          <Row className="py-3 justify-content-center">
+            <Col xs="12" sm="10" md="8" lg="8">
+              <Button color="primary">
+                Transfer
+              </Button>
+            </Col>
+          </Row>
+        </div>
       );
     }
 
     return (
-      <ListGroup>
-        <ListGroupItem>
-          <div className="placeholder"></div>
-        </ListGroupItem>
-        <ListGroupItem>
-          <div className="placeholder"></div>
-        </ListGroupItem>
-        <ListGroupItem>
-          <div className="placeholder"></div>
-        </ListGroupItem>
-        <ListGroupItem>
-          <div className="placeholder"></div>
-        </ListGroupItem>
-        <ListGroupItem>
-          <div className="placeholder"></div>
-        </ListGroupItem>
-      </ListGroup>
+      <Row className="py-3">
+        <Col>
+          <Card>
+            <CardBody>
+              <ListGroup>
+                <ListGroupItem>
+                  <div className="placeholder" />
+                </ListGroupItem>
+                <ListGroupItem>
+                  <div className="placeholder" />
+                </ListGroupItem>
+                <ListGroupItem>
+                  <div className="placeholder" />
+                </ListGroupItem>
+                <ListGroupItem>
+                  <div className="placeholder" />
+                </ListGroupItem>
+                <ListGroupItem>
+                  <div className="placeholder" />
+                </ListGroupItem>
+              </ListGroup>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
     );
   }
 
   render = () => (
     <Container fluid>
-      <Row className="py-3">
-        <Col>
-          <Card>
-            <CardBody>
-              {this.displayCard()}
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+      {this.displayCard()}
     </Container>
   );
 }
