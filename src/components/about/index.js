46c102b5728c84dc1de3d288be72ca6a3ca5e1b7
Clement Lakhal <clementlakhal@gmail.com>
2018-12-26 02:58:33 +0100

    Updated placeholder

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -7,10 +7,19 @@ import {
 
 const About = () => (
   <Container>
-    <Row>
-      <Col>
+    <Row className="py-3 justify-content-center">
+      <Col xs="12" sm="10" md="10" lg="10">
         <h1>About us</h1>
-        <p>Our story</p>
+        <p className="lead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </Col>
+    </Row>
+    <Row className="py-3 justify-content-center">
+      <Col xs="12" sm="10" md="10" lg="10">
+        <p>
+          Quisque auctor hendrerit tempor. Pellentesque aliquam est eu nisl egestas, pulvinar tincidunt felis varius. Duis in eleifend metus. Fusce scelerisque, nunc at ultricies posuere, mi est mollis urna, vel pharetra mauris ipsum sed augue. Suspendisse orci risus, auctor et tellus eu, laoreet condimentum massa. Cras elementum viverra mauris, in hendrerit nisl rhoncus congue. Proin tristique fringilla mauris. Quisque pulvinar, eros eget bibendum elementum, enim est varius sem, id posuere sapien odio nec orci.
+        </p>
       </Col>
     </Row>
   </Container>
--- src/components/team/index.js
+++ src/components/team/index.js
@@ -7,10 +7,19 @@ import {
 
 const Team = () => (
   <Container>
-    <Row>
-      <Col>
-        <h1>Team</h1>
-        <p>Our team</p>
+    <Row className="py-3 justify-content-center">
+      <Col xs="12" sm="10" md="10" lg="10">
+        <h1>Our team</h1>
+        <p className="lead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </Col>
+    </Row>
+    <Row className="py-3 justify-content-center">
+      <Col xs="12" sm="10" md="10" lg="10">
+        <p>
+          Quisque auctor hendrerit tempor. Pellentesque aliquam est eu nisl egestas, pulvinar tincidunt felis varius. Duis in eleifend metus. Fusce scelerisque, nunc at ultricies posuere, mi est mollis urna, vel pharetra mauris ipsum sed augue. Suspendisse orci risus, auctor et tellus eu, laoreet condimentum massa. Cras elementum viverra mauris, in hendrerit nisl rhoncus congue. Proin tristique fringilla mauris. Quisque pulvinar, eros eget bibendum elementum, enim est varius sem, id posuere sapien odio nec orci.
+        </p>
       </Col>
     </Row>
   </Container>
